4952f0742ac6771b07ba159cc655304e0d3fe1cf
Mrxiaoling <ling1471626451@Gmail.com>
2019-02-23 20:18:15 +0800

    界面动画，监听，引导界面-缓存，布局

--- app/src/main/java/com/example/beijingnews/acitity/GuideActivity.java
+++ app/src/main/java/com/example/beijingnews/acitity/GuideActivity.java
@@ -1,13 +1,15 @@
-package com.example.beijingnews;
+package com.example.beijingnews.acitity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.beijingnews.R;
+
+public class GuideActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_guide_activy);
     }
 }
---
+++ app/src/main/java/com/example/beijingnews/utils/CahcheUtils.java
@@ -0,0 +1,22 @@
+package com.example.beijingnews.utils;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+/**
+ * 缓存工具类，用来判断是否进入过主页面
+ */
+public class CahcheUtils {
+    /**
+     *
+     * @param context上下文
+     *
+     * @param key key值
+     * @return
+     */
+    public static boolean getBoolean (Context context,String key){
+
+        SharedPreferences sp = context.getSharedPreferences("ling", Context.MODE_PRIVATE);
+        return sp.getBoolean(key, false);
+    }
+}
