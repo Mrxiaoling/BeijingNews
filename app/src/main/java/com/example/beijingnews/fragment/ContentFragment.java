db47897eb6ef96481a22dda66439e338ea2d3e3f
Mrxiaoling <ling1471626451@Gmail.com>
2019-02-24 13:14:08 +0800

    主页面正文Fragment的布局，默认选中主页面

--- app/src/main/java/com/example/beijingnews/fragment/ContentFragment.java
+++ app/src/main/java/com/example/beijingnews/fragment/ContentFragment.java
@@ -1,17 +1,29 @@
 package com.example.beijingnews.fragment;
 
 import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
 import android.view.View;
+import android.widget.RadioGroup;
+
+import com.example.beijingnews.R;
 
 public class ContentFragment extends BaseFragment {
 
+    private ViewPager viewPager;
+    private RadioGroup rg_main;
+
     @Override
     public View initView() {
-        return null;
+        View view = View.inflate(context, R.layout.content_fragment, null);
+        viewPager = view.findViewById(R.id.viewpager);
+        rg_main = view.findViewById(R.id.rg_main);
+        return view;
     }
 
     @Override
     public void initData() {
         super.initData();
+        //设置默认选中首页
+        rg_main.check(R.id.rb_home);
     }
 }
